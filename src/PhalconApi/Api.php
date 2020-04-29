0a69785673ff7bff830fa2ec67b3f19abd70d50c
Bart Blok <bart@wittig.nl>
2020-04-29 14:46:24 +0200

    API now implemented ACL Mountable interface

--- src/PhalconApi/Api.php
+++ src/PhalconApi/Api.php
@@ -2,8 +2,11 @@
 
 namespace PhalconApi;
 
+use Phalcon\Acl\Enum as AclEnum;
 use Phalcon\Mvc\Micro;
+use Phalcon\Mvc\Micro\CollectionInterface;
 use PhalconApi\Constants\Services;
+use PhalconApi\Acl\MountableInterface;
 
 /**
  * Class Api
@@ -19,8 +22,10 @@ use PhalconApi\Constants\Services;
  * @property \PhalconApi\Data\Query $query
  * @property \PhalconApi\Data\Query\QueryParsers\UrlQueryParser $urlQueryParser
  */
-class Api extends Micro
+class Api extends Micro implements MountableInterface
 {
+    protected $_collections = [];
+
     /**
      * Attaches middleware to the API
      *
@@ -37,5 +42,46 @@ class Api extends Micro
         $this->getEventsManager()->attach('micro', $middleware);
 
         return $this;
+    }
+
+    public function mount(CollectionInterface $collection): Micro
+    {
+        $this->_collections[] = $collection;
+        return parent::mount($collection);
+    }
+
+    public function getAclResources()
+    {
+        $resources = [];
+
+        foreach ($this->_collections as $collection){
+
+            if($collection instanceof MountableInterface){
+                $resources = array_merge($resources, $collection->getAclResources());
+            }
+        }
+
+        return $resources;
+    }
+
+    public function getAclRules(array $roles)
+    {
+        $allowed = [];
+        $denied = [];
+
+        foreach ($this->_collections as $collection){
+
+            if($collection instanceof MountableInterface){
+
+                $rules = $collection->getAclRules($roles);
+                $allowed = array_merge($allowed, $rules[AclEnum::ALLOW]);
+                $denied = array_merge($denied, $rules[AclEnum::DENY]);
+            }
+        }
+
+        return [
+            AclEnum::ALLOW => $allowed,
+            AclEnum::DENY => $denied
+        ];
     }
 }
